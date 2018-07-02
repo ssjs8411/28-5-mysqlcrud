174e798ec25aad1e7a17330e2645d66b5373cd46
Administrator <Administrator@smart20118>
2018-07-02 16:04:53 +0900

    [최지수] select List 만들기
    1. StudentDao 클래스 내 select 메서드 선언
     - selectCount, selectStudentByPage 메서드
    2. studentList.jsp생성
     - 위에 만든 메서드 2개 호출
     - 전체 리스트 조회 및 페이징작업

---
+++ 28-5-mysqlcrud/WebContent/Student/studentList.jsp
@@ -0,0 +1,71 @@
+<!-- 18.7.2 최지수 -->
+<%@ page language="java" contentType="text/html; charset=EUC-KR" pageEncoding="EUC-KR"%>
+<%@ page import = "service.StudentDao" %>
+<%@ page import = "java.util.ArrayList" %>
+<%@ page import = "service.Student" %>
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=EUC-KR">
+		<title>Insert title here</title>
+	</head>
+	<body>
+		<table>
+			<tr>
+				<th>번호</th>
+				<th>이름</th>
+				<th>나이</th>
+			</tr>
+<%
+	request.setCharacterEncoding("euc-kr");
+	
+	int currentPage = 1;	// 페이지 번호
+	int rowPerPage = 1;		// 한 페이지당 보는 갯수
+			
+	if(request.getParameter("currentPage") !=null ){
+			currentPage = Integer.parseInt(request.getParameter("currentPage"));
+	}
+	
+	int startRow = (currentPage - 1) * rowPerPage;	// 시작번호 = (페이지번호-1) * 한 페이지당 보는 갯수
+			
+	StudentDao sdao = new StudentDao();		// StudentDao 클래스 연결
+	ArrayList<Student> list = sdao.selectStudentByPage(startRow, rowPerPage);	// StudentDao 클래스 내 selectStudentByPage메서드 호출(매개변수 시작번호, 한페이지당 보는갯수)
+	
+	for(int i=0; i<list.size(); i++){
+		Student s = list.get(i);
+%>
+		
+			<tr>
+				<td><%= s.getStudent_no() %></td>
+				<td><%= s.getStudent_name() %></td>
+				<td><%= s.getStudent_age() %></td>
+			</tr>	
+<%		
+	}
+%>
+		</table>
+<%	
+	int totalRow = sdao.selectCount();	// 전체 갯수
+	int lastPage = 0;	// 마지막페이지
+	
+	if(totalRow % rowPerPage == 0){		// 총 갯수 % 한페이지당 보는 갯수
+		lastPage = totalRow / rowPerPage ;		
+	        // 마지막페이지 = 총 갯수 / 한페이지당 보는 갯수
+	}else{
+		lastPage = totalRow / rowPerPage + 1 ;	
+	       // 마지막페이지 = 총 갯수 / 한페이지당 보는 갯수 + 1
+	}
+				
+	if(currentPage > 1){
+%>
+		<a href="<%=request.getContextPath() %>/Student/studentList.jsp?currentPage=<%=currentPage-1 %>">이전</a>
+<%
+	}
+	if(currentPage < lastPage){
+%>
+		<a href="<%=request.getContextPath() %>/Student/studentList.jsp?currentPage=<%=currentPage+1%>">다음</a>
+<%
+	}
+%>
+	</body>
+</html>
--- 28-5-mysqlcrud/src/service/StudentDao.java
+++ 28-5-mysqlcrud/src/service/StudentDao.java
@@ -4,9 +4,112 @@ package service;						// service 패키지 내에 있다
 import java.sql.Connection;				// Connection 사용위해 import
 import java.sql.DriverManager;			// DriverManager 사용위해 import 
 import java.sql.PreparedStatement;		// PreparedStatement 사용위해 import 
+import java.sql.ResultSet;
 import java.sql.SQLException;			// 오류검사 위해 import
+import java.util.ArrayList;
 
 public class StudentDao {				// StudentDao 클래스
+	
+	public int selectCount() {
+		
+		// 테이블 내의 데이터 갯수를 구하기 위한 메서드
+		// 숫자값을 리턴하기 위해 return data type 은 int type
+		
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		int totalRow = 0;
+		
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			
+			String dbDriver = "jdbc:mysql://localhost:3306/5mysqlcrud?useUnicode=true&characterEncoding=euckr";
+			String dbUser = "root";
+			String dbPass = "java0000";
+			conn = DriverManager.getConnection(dbDriver, dbUser, dbPass);
+			
+			pstmt = conn.prepareStatement("SELECT COUNT(*) FROM student");
+			
+			rs = pstmt.executeQuery();
+			
+			if(rs.next()) {
+				totalRow = rs.getInt("COUNT(*)");
+			}
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}finally {
+			
+			// 객체 종료(실행순서 거꾸로 종료시켜준다)
+			if(rs!=null) try{ rs.close(); } catch (SQLException e) {}
+			if(pstmt!=null) try{ pstmt.close(); } catch (SQLException e) {}	// 쿼리연결종료
+			if(conn!=null) try{ conn.close(); } catch (SQLException e) {}	// DB연결종료
+			
+		}
+		
+		
+		return totalRow;
+		
+	}
+	
+	public ArrayList<Student> selectStudentByPage(int page, int pagePerRow){
+		
+		// 테이블 내 전체 학생의 수를 구하기 위한 메서드
+		// ArrayList type으로 Student클래스의 주소값 리턴(배열)
+		// 쿼리문의 몇번 부터 몇개까지 출력할건지 입력위해 매개변수 2개 전부 int type
+		
+		ArrayList<Student> list = new ArrayList<>();
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		
+		String sql = "SELECT student_no, student_name, student_age FROM student ORDER BY student_no LIMIT ?,?";	// student에서 student_no칼럼 기준 몇번부터 몇개까지 조회
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			
+			String dbDriver = "jdbc:mysql://localhost:3306/5mysqlcrud?useUnicode=true&characterEncoding=euckr";
+			String dbUser = "root";
+			String dbPass = "java0000";
+			conn = DriverManager.getConnection(dbDriver, dbUser, dbPass);
+			
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setInt(1, page);
+			pstmt.setInt(2, pagePerRow);
+			
+			rs = pstmt.executeQuery();
+			
+			while(rs.next()) {	// 조회하여 값이 출력될때까지 반복
+				
+				Student s = new Student();
+				
+				s.setStudent_no(rs.getInt("student_no"));
+				s.setStudent_name(rs.getString("student_name"));
+				s.setStudent_age(rs.getInt("student_age"));
+				
+				list.add(s);
+			}
+			
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}finally {
+			
+			// 객체 종료(실행순서 거꾸로 종료시켜준다)
+			if(rs!=null) try{ rs.close(); } catch (SQLException e) {}
+			if(pstmt!=null) try{ pstmt.close(); } catch (SQLException e) {}	// 쿼리연결종료
+			if(conn!=null) try{ conn.close(); } catch (SQLException e) {}	// DB연결종료
+			
+		}
+		
+		return list;	// list 최대 pageperRow~1
+		
+	}
 	
 
 	public int insertStudent(Student s)  {			// 접근지정자 public(모든곳에서 접근가능) 
